39b4b6ba35ce8a3dc79be8b6d4c9b9d166d744a7
Arleta Jędrzejczak <Arleta.Jedrzejczak@ttpsc.pl>
2019-11-17 18:17:25 +0100

    Updates

---
+++ precache-manifest.f857e08587ef49de0c447ea5deda4d46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4169490b1559d72b4c29a1c5bea0a425",
+    "url": "/tic-tac-toe-react/index.html"
+  },
+  {
+    "revision": "3f0cc0859f76e9f38d01",
+    "url": "/tic-tac-toe-react/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "a71e96abaac9870203e3",
+    "url": "/tic-tac-toe-react/static/css/main.4e2cceff.chunk.css"
+  },
+  {
+    "revision": "3f0cc0859f76e9f38d01",
+    "url": "/tic-tac-toe-react/static/js/2.670793d7.chunk.js"
+  },
+  {
+    "revision": "a71e96abaac9870203e3",
+    "url": "/tic-tac-toe-react/static/js/main.74b6e6da.chunk.js"
+  },
+  {
+    "revision": "e771b0da652a645ee676",
+    "url": "/tic-tac-toe-react/static/js/runtime-main.b9576222.js"
+  }
+]);
